54c92dc977c23cb6bcd655a611cc1224cebcb2ea
Kris Herbert <klh55@pct.edu>
2024-11-05 13:27:15 -0500

    Modified coin model and began to code

---
+++ Assets/Scripts/coin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+//TODO:
+//Spawn coin
+//make it flip and move
+//allow player to toss it
+//have it despwan after an amount of time
+//add cooldown
