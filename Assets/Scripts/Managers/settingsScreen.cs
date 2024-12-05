3bb7d985be2d3c5632809a7807714def9109c531
Kris Herbert <klh55@pct.edu>
2024-12-05 13:32:31 -0500

    Main menu buttons and Settings Screen continued

--- Assets/Scripts/Managers/settingsScreen.cs
+++ Assets/Scripts/Managers/settingsScreen.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class settingsScreen : MonoBehaviour
 {
+    public string mainMenuScene;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void ButtonReturn()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    // TODO: Disable/Enable chromatic Abberation, FOV slider, sound slider
 }
